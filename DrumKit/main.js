6c199e9a62a99361d20b273228d915174146045d
Brittany Keith <bkinux@gmail.com>
2023-05-08 13:21:18 +0200

    final

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -1,23 +1,18 @@
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  e.target.classList.remove("playing");
-}
-
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //animation
   if (!audio) return; //stop the function from running all together
-
-  key.classList.add("playing");
   audio.currentTime = 0; //rewind to the start
   audio.play();
+  key.classList.add("playing");
 }
 
-function removeClassList(e) {
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  key.removeClassList("playing");
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+  console.log("removed");
 }
 
-const keys = Array.from(document.querySelectorAll(".key"));
+const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
